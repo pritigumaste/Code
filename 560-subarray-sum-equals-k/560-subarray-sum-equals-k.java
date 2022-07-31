01f0828146e4920316317e086fa15ea389aa67ab
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2022-07-31 15:27:18 +0530

    Time: 60 ms (21.50%), Space: 72.8 MB (5.36%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int count=0; //counter to count the sub-arrays
+        int sum=0; 
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            sum += nums[i];
+            
+            if(map.containsKey(sum- k))
+                count = count + map.get(sum-k);
+                
+            if(!map.containsKey(sum)){
+                map.put(sum, 0);
+            }
+            map.put(sum, map.get(sum) + 1);
+            
+        }
+        
+      return count; 
+    }
+}
